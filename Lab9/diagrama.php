e5a24aefee6f219595703ace409f2f1b1a985afc
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-12-15 17:04:00 +0200

    Solve histogram exe

--- Lab9/diagrama.php
+++ Lab9/diagrama.php
@@ -3,25 +3,29 @@
 require dirname(__FILE__) . '/../dblogin.secret.php';
 $conn = new mysqli("localhost", $dbuser, $dbpass, "hr");
 require 'jpgraph/src/jpgraph.php';
-require 'jpgraph/src/jpgraph_pie.php';
-require 'jpgraph/src/jpgraph_pie3d.php';
-$sql = "SELECT department_id, count(employee_id) as angajati from employees 
-where department_id is not null GROUP by department_id having count(employee_id)>0;";
+require 'jpgraph/src/jpgraph_bar.php';
+$sql = "SELECT job_id, count(employee_id) as angajati from employees 
+where job_id is not null GROUP by job_id having count(employee_id)>0;";
 $result = $conn->query($sql);
 $num_results = $result->num_rows;
 $joburi = array();
 $angajati = array();
+$list = "";
+$list .= '<ul>';
 for ($i=0; $i <$num_results; $i++) {
    $row = $result->fetch_assoc();
-   array_push($joburi,'Dep'.$row["department_id"].' ');
+   array_push($joburi,'Job '.$row["job_id"].' ');
    array_push($angajati,intval($row["angajati"]));
-   echo 'Dep'.$row["department_id"]."   ".$row["angajati"].'# ';}
+   $list .= '<li>Job'.$row["job_id"]."   ".$row["angajati"].'# </li>';
+}
+$list .=  '</ul>';
 
 $data =[40,60,25,34];
 
-$graph = new PieGraph(960,660);
+$graph = new Graph(960,660);
 
 $theme_class= new VividTheme;
+$graph->SetScale("textlin");
 $graph->SetTheme($theme_class);
 $graph->SetShadow();
 
@@ -29,10 +33,9 @@ $graph->title->Set('A simple 3D Pie plot');
 $graph->title->SetFont(FF_FONT1,FS_BOLD);
 
 
-$p1 = new PiePlot3D($angajati);
-$p1->ExplodeSlice(1);
+$p1 = new BarPlot($angajati);
 $p1->SetCenter(0.5);
-$p1->SetLegends($joburi);
+$graph->xaxis->SetTickLabels($joburi);
 $graph->legend->Pos(.088,0.9);
 
 $graph->Add($p1);
@@ -50,3 +53,4 @@ $imageData = base64_encode(ob_get_contents());
 ob_end_clean();
 
 echo '<img src="data:image/png;base64,'.$imageData.'">';
+echo $list;
